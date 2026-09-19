cb838de2e5125dab2ed48dd9b26818bb29bce004
Sumitra Dawn <Sdawn30>
2026-09-19 15:58:28 +0530

    Basic_functions

---
+++ func.py
@@ -0,0 +1,41 @@
+#Error while running need to add paranthesis post function call
+
+def Hello_world1():
+    pass
+
+print(Hello_world1)
+
+
+#simple function without doing anything prints "none"
+
+def Hello_world2():
+    pass
+
+print(Hello_world2())
+
+
+#function to print hello world directly in function
+
+def Hello_world3():
+   print("hello world3")
+
+Hello_world3()
+
+
+#function to print hello world with return but here need to print
+
+def Hello_world3():
+   return("hello world3")
+
+print(Hello_world3())
+
+
+#use inbuilt function in my function
+
+def Hello_world4():
+   return("hello world4")
+
+print(Hello_world4().upper())
+
+
+
